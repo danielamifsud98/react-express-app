62c6af20644c2adbaaedad770bf1f9b84a776ba4
danielamifsud98 <38285073+danielamifsud98@users.noreply.github.com>
2020-04-25 13:57:34 +0200

    Started working on Navbar.js

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => (
+    <nav className="navbar navbar-expand-lg navbar-dark dark-color">
+        <Link className="nav-link navbar-title" to="/">My Assignment</Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+        </button>
+    
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                    <Link className="nav-link" to="/question-1">Question 1</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/question-2">Question 2</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/question-3">Question 3</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/question-4">Question 4</Link>
+                </li>
+                <li className="nav-item dropdown">
+                    <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        SQL
+                    </a>
+                    <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a className="dropdown-item" href="#">Question 1</a>
+                        <div className="dropdown-divider"></div>
+                        <a className="dropdown-item" href="#">Question 2</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </nav>
+)
+
+export default Navbar;
--- server/src/server.js
+++ server/src/server.js
@@ -2,9 +2,20 @@ import express from 'express';
 import bodyParser from 'body-parser';
 
 const app = express();
-// adding a comment to test commit --
+const axios = require('axios');
 
 app.use(bodyParser.json());
+
+// QUESTION 1
+app.get('/api/country/specified-name', async (req,res) => {
+    axios.get('https://restcountries.eu/rest/v2/name/Malta')
+    .then(response => {
+        res.status(200).json(response.data);
+    })
+    .catch(error => {
+        res.status(500).json({message: "There was an error!", error});
+    });
+})
 
 app.get('/hello', (req, res) => res.send('Hello!'));
 
