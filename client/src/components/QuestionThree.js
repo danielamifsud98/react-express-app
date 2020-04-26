92c34c1056d606d626ad457a697e7cb6faf3c4ef
danielamifsud98 <38285073+danielamifsud98@users.noreply.github.com>
2020-04-26 11:42:22 +0200

    Finished Question 3

--- client/src/components/QuestionThree.js
+++ client/src/components/QuestionThree.js
@@ -1,23 +1,52 @@
 import React, {useState, useEffect} from 'react';
+import CountryResults from './CountryResults';
 
 const QuestionThree = () => {
+    // SET COUNTRIES AND FILTER NAME
     const [countries, setCountries] = useState([]);
+    const [countryName, setCountryName] = useState('');
+    const [filteredCountries, setFilteredCountries] = useState([]);
+
+    // GET ALL COUNTRIES
     useEffect(() => {
         const fetchData = async () => {
             const result = await fetch(`/api/country/all`);
             const body = await result.json();
-            setCountries(body);
-            console.log(body);
+            setCountries(body);   
+            setFilteredCountries(body);         
         }
         fetchData();
     }, []);
 
+    // FILTER COUNTRIES
+    useEffect(() => {
+        const results = countries.filter(country =>
+            country.name.toLowerCase().includes(countryName.toLowerCase().trim())
+        );
+        setFilteredCountries(results);
+    }, [countryName]);
+
     return (
-        <React.Fragment>
-            {countries.map((country) => (
-                <span key={country.name} className="badge badge-pill badge-info">{country.name}</span>
-            ))}
-        </React.Fragment>
+        <>
+            <div className="form-group form-group-question">
+                <label className="font-weight-bold">Filter through the list of countries:</label>
+                <div className="row">
+                    <div className="col col-12">
+                        <input 
+                            type="text" 
+                            className="form-control" 
+                            value={countryName} 
+                            onChange={(event) => setCountryName(event.target.value)} 
+                            placeholder="Insert Country Name"/>
+                    </div>
+                </div>
+            </div>            
+            <CountryResults countryInformation={
+                filteredCountries.map((country) => (
+                    <span key={country.name} className="badge badge-pill badge-light custom-pill">{country.name}</span>
+                ))
+            }/>
+        </>
     )
 }
 
